f38508744917fd795666b7bfa45fcca98d0ef18b
MathieuNassar <mathieu.nassar@gmail.com>
2019-09-27 23:23:54 +0200

    FIX: CI osx missing deps + upload

--- .ci/build.py
+++ .ci/build.py
@@ -2,6 +2,6 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(upload_dependencies="all", build_policy="missing")
     builder.add_common_builds(shared_option_name="ghostmodule:shared")
     builder.run()
